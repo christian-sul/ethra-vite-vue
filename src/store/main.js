5a31306beb8ac6f09ca10963c1ee1d68da510986
Delvin <work.d6lv9n.github@gmail.com>
2025-10-26 00:58:10 +0700

    Add basic vuex configuration

---
+++ src/store/main.js
@@ -0,0 +1,27 @@
+import { createStore } from 'vuex';
+
+// Import modules here...
+// import .. from '';
+
+// Modules
+const modules = {
+    // 
+};
+
+// Create store instance
+const store = createStore({
+    strict: true,
+    modules
+});
+
+// Subscribe to mutations
+// store.subscribe((mutation, state) => {
+//     // 
+// });
+
+// Subscribe to actions
+// store.subscribeAction((action, state) => {
+//     // 
+// });
+
+export default store;
