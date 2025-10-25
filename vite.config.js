ff45fd4d3adb5dc1c901312d18ad4b06bc03bd3c
Delvin <work.d6lv9n.github@gmail.com>
2025-10-26 00:46:34 +0700

    Update vite.config.js to integrate with tailwindcss, vue devtools, and add file path alias '@'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+// export default defineConfig({
+//   plugins: [vue()],
+// });
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      tailwindcss(),
+      vue(),
+      vueDevTools({
+        // appendTo: '',
+        componentInspector: true,
+        launchEditor: 'visualstudio'
+      })
+    ],
+    resolve: {
+      alias: {
+        // '@': path.resolve(__dirname, './src'),
+        '@': fileURLToPath(new URL('./', import.meta.url)),
+      }
+    }
+  }
+});
