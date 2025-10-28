a2a87047e7bcc2ea790b1ef2e514ed3f389d0b9a
Delvin <work.d6lv9n.github@gmail.com>
2025-10-28 22:51:19 +0700

    Update playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /** Default was 'html'; opens browser */
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off', // trace: 'on-first-retry',
+    screenshot: 'off',
+    video: 'off',
+    headless: true, // headless: false,
   },
 
   /* Configure projects for major browsers */
